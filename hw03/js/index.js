f75be43d709639458cb05227d255e3a7b4b9f5f7
vbass <32740596+vbass0903@users.noreply.github.com>
2021-05-10 18:23:27 -0500

    Added hw03

---
+++ hw03/js/index.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ * -------------------------------------
+ * DOM Manipulation / Traversal Activity
+ * -------------------------------------
+ * 
+ * 1. Create and attach an event handler (function) to each ".image" 
+ * element so that when the ".image" element is clicked, the corresponding 
+ * image loads in the .featured image element.
+ * 
+ * 2. Create event handlers for the next and previous buttons. The next button should
+ *    show the next image in the thumbnail list. The previous should show the previous.
+ * 
+ * 3. If you get to the end, start at the beginning. 
+ * 
+ * 4. If you get to the beginning, loop around to the end.
+ * 
+ * 
+ */
+let photoID = 0;
+
+const images = [
+    'images/field1.jpg',
+    'images/purple.jpg',
+    'images/jar.jpg',
+    'images/green.jpg',
+    'images/green1.jpg',
+    'images/purple1.jpg',
+    'images/magnolias.jpg',
+    'images/daisy1.jpg'
+];
+
+const initScreen = () => {
+    images.forEach((image, idx) => {
+        document.querySelector('.cards').innerHTML += `
+            <li class="card">
+                <div class="image"
+                    onclick="thumbnailClick(event);" 
+                    style="background-image:url('${image}')"
+                    data-index="${idx}"></div>
+            </li>`;
+    });
+};
+
+initScreen();
+
+const thumbnailClick = (ev) => {
+    //console.log(ev.target.style.backgroundImage)
+    document.querySelector('.featured_image').style.backgroundImage = ev.target.style.backgroundImage;
+}
+
+const changePicture = (ev) => {
+    const object = ev.target.className
+    if (object === 'next' || object === 'featured_image') {
+        if (photoID < (images.length - 1)) {
+            photoID = photoID + 1;
+            document.querySelector('.featured_image').style.backgroundImage = `url('${images[photoID]}')`
+        }
+    }
+    else {
+        if (photoID > 0) {
+            photoID = photoID - 1;
+            document.querySelector('.featured_image').style.backgroundImage = `url('${images[photoID]}')`
+        }
+    }
+}
