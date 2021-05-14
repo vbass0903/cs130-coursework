d763fcd7f333379156f98cfb1cb561f9d1d72853
vbass0903 <32740596+vbass0903@users.noreply.github.com>
2021-05-14 18:20:23 -0500

    Finished hw04

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -4,6 +4,14 @@ const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 const audioFile = 'https://p.scdn.co/mp3-preview/bfead324ff26bdd67bb793114f7ad3a7b328a48e?cid=9697a3a271d24deea38f8b7fbfa0e13c';
 const audioPlayer = AudioPlayer('.player', audioFile);
 
+const playTrack = (ev) => {
+    audioPlayer.setAudioFile(ev.currentTarget.dataset.previewTrack)
+    const image = ev.currentTarget.children[0].src
+    document.querySelector('footer .track-item').innerHTML = `<img src=${image}>`
+    audioPlayer.play()
+}
+
+
 const search = (ev) => {
     const term = document.querySelector('#search').value;
     console.log('search for:', term);
@@ -24,35 +32,61 @@ const getTracks = (term) => {
         }
     )
     .then(tracks => {
-        tracks = tracks[0]
         document.querySelector('#tracks').innerHTML = ""
-        for (track in tracks) {
-            document.querySelector('#tracks').innerHTML +=
-            `<section class="track-item preview" data-preview-track="${track.preview_url}">
-                 <img src="${track.album.image_url}">
-                 <i class="fas play-track fa-play" aria-hidden="true"></i>
-                 <div class="label">
-                     <h3>${track.name}</h3>
-                     <p>
-                        ${track.artist.name}
-                     </p>
-                 </div>
-             </section>`
+        if (tracks.length == 0) {
+            document.querySelector('#tracks').innerHTML = "<h3>No tracks found</h3>"
         }
-        
+        else {
+            for (let i = 0; i < tracks.length; i++) {
+                document.querySelector('#tracks').innerHTML +=
+                `<section onclick="playTrack(event)" class="track-item preview" data-preview-track="${tracks[i].preview_url}">
+                     <img src="${tracks[i].album.image_url}">
+                     <i class="fas play-track fa-play" aria-hidden="true"></i>
+                     <div class="label">
+                         <h3>${tracks[i].name}</h3>
+                         <p>
+                            ${tracks[i].artist.name}
+                         </p>
+                     </div>
+                 </section>`
+            }
+        }        
     }
 
     )
 };
 
 const getAlbums = (term) => {
-//     fetch(baseURL + `?type=album&q=${term}`)
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    fetch(baseURL + `?type=album&q=${term}`)
+    .then(
+        response => {
+            return (response.json())
+        }
+    )
+    .then(albums => {
+        document.querySelector('#albums').innerHTML = ""
+        if (albums.length == 0) {
+            document.querySelector('#albums').innerHTML = "<h3>No albums found</h3>"
+        }
+        else {
+            for (let i = 0; i < albums.length; i++) {
+                document.querySelector('#albums').innerHTML +=
+                `<section class="album-card" id="${albums[i].id}">
+                    <div>
+                        <img src="${albums[i].image_url}">
+                        <h3>${albums[i].name}</h3>
+                        <div class="footer">
+                            <a href="${albums[i].spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`
+            }
+        }        
+    }
+
+    )
 };
 
 const getArtist = (term) => {
@@ -63,19 +97,24 @@ const getArtist = (term) => {
             }
         )
         .then (artist => {
-            artist = artist[0]
-            document.querySelector('#artist').innerHTML =
-            `<section class="artist-card" id="${artist.id}">
-                <div>
-                    <img src="${artist.image_url}">
-                    <h3>${artist.name}</h3>
-                    <div class="footer">
-                        <a href="${artist.spotify_url}" target="_blank">
-                            view on spotify
-                        </a>
+            if (artist.length == 0) {
+                document.querySelector('#artist').innerHTML = "<h3>Artist not found</h3>"
+            }
+            else {
+                artist = artist[0]
+                document.querySelector('#artist').innerHTML =
+                `<section class="artist-card" id="${artist.id}">
+                    <div>
+                        <img src="${artist.image_url}">
+                        <h3>${artist.name}</h3>
+                        <div class="footer">
+                            <a href="${artist.spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
                     </div>
-                </div>
-            </section>`
+                </section>`
+            }
         })
 };
 
